cefe053cdf377f16153afd88a6b3c71e63d8e874
matteonussbauemer <m.l.nussbaumer@student.tudelft.nl>
2024-04-21 17:16:59 +0200

    clean up code: - put folder setup and container launch in separate methods - set up folder right before launching container - use container name as folder name - extract base path as constant

--- launch.py
+++ launch.py
@@ -1,31 +1,27 @@
 import os
 import docker
 
-# 1. Create folders to be mounted by the containers
-folders = ["ubuntu_18", "ubuntu_22", "fedora"]
-
-for folder in folders:
-    folder_path = os.path.join(os.getcwd(), folder)
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-        print(f"Folder {folder} created at {folder_path}")
-    else:
-        print(f"Folder {folder} already exists at {folder_path}")
-
-# 2. Create a Docker client 
-client = docker.from_env() 
-
-# 3. Define the configurations for the containers and launch 
-
+# 2. Create a Docker client
+client = docker.from_env()
+BASE_PATH = os.getcwd()
+# 3. Define the configurations for the containers
 container_configs = [
-    {"image": "ubuntu:18.04", "name": "ubuntu_18", "mount_path": f"{os.getcwd()}/ubuntu_18"},
-    {"image": "ubuntu:22.04", "name": "ubuntu_22", "mount_path": f"{os.getcwd()}/ubuntu_22"},
-    {"image": "fedora", "name": "fedora", "mount_path": f"{os.getcwd()}/fedora"}
+    {"image": "ubuntu:18.04", "name": "ubuntu_18", "mount_path": f"{BASE_PATH}/ubuntu_18"},
+    {"image": "ubuntu:22.04", "name": "ubuntu_22", "mount_path": f"{BASE_PATH}/ubuntu_22"},
+    {"image": "fedora", "name": "fedora", "mount_path": f"{BASE_PATH}/fedora"}
 ]
 
-# Launch the containers
-# tty and command are used to keep the container running: https://stackoverflow.com/a/54623344/14684936 
-for config in container_configs:
+
+def create_container_folder(folder_name: str):
+    folder_path = os.path.join(BASE_PATH, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder {folder_name} created at {folder_path}")
+    else:
+        print(f"Folder {folder_name} already exists at {folder_path}")
+
+
+def launch_container(config):
     container = client.containers.run(
         config["image"],
         detach=True,
@@ -36,3 +32,14 @@ for config in container_configs:
         tty=True
     )
     print(f"Container {config['name']} launched with ID {container.id}")
+
+
+def launch_all_containers():
+    # Launch the containers
+    # tty and command are used to keep the container running: https://stackoverflow.com/a/54623344/14684936
+    for config in container_configs:
+        create_container_folder(config['name'])
+        launch_container(config)
+
+
+launch_all_containers()
