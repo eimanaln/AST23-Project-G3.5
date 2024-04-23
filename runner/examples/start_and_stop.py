6ff490d6b74052284b7d5acf9cc1d913021dbb8a
matteonussbauemer <m.l.nussbaumer@student.tudelft.nl>
2024-04-23 11:29:49 +0200

    add post_init_commands to ubuntu container

--- runner/examples/start_and_stop.py
+++ runner/examples/start_and_stop.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # Define the configurations for the containers
     container_configs: list[ContainerConfiguration] = [
         ContainerConfiguration(image="ubuntu:18.04", name="ubuntu_18", mount_path=f"{BASE_PATH}/ubuntu_18", post_init_commands=["apt-get update", "apt-get install -y python3", "ln -s /usr/bin/python3 /usr/bin/python"]),
-        ContainerConfiguration(image="ubuntu:22.04", name="ubuntu_22", mount_path=f"{BASE_PATH}/ubuntu_22"),
+        ContainerConfiguration(image="ubuntu:22.04", name="ubuntu_22", mount_path=f"{BASE_PATH}/ubuntu_22", post_init_commands=["apt-get update", "apt-get install -y python3", "ln -s /usr/bin/python3 /usr/bin/python"]),
         ContainerConfiguration(image="fedora", name="fedora", mount_path=f"{BASE_PATH}/fedora")
     ]
     manager.launch_all_containers(container_configs=container_configs)
