8a88e2d7f470e8e8aa85e162a5b381fd6d161abd
matteonussbauemer <m.l.nussbaumer@student.tudelft.nl>
2024-05-04 15:10:28 +0200

    adapt the DockerContainerManager to generate hosts via a python generator

--- runner/container_runner.py
+++ runner/container_runner.py
@@ -1,14 +1,20 @@
 import os
+import shutil
+from typing import Generator
+
 import docker
 from docker.models.containers import Container
 
 from runner.container_configuration import ContainerConfiguration
+from runner.host import Host
+
 
 class DockerContainerManager:
-    def __init__(self):
+    def __init__(self, contaier_configs: list[ContainerConfiguration] = []):
         # Create a Docker client
         self.client = docker.from_env()
-        self.running_containers = []
+        self.running_containers: dict[str, ContainerConfiguration] = {}
+        self.container_configs = contaier_configs
 
     def create_container_folder(self, folder_path: str):
         if not os.path.exists(folder_path):
@@ -23,7 +29,7 @@ class DockerContainerManager:
         except docker.errors.NotFound:
             return None
 
-    def launch_container(self, config: ContainerConfiguration) -> Container:
+    def launch_container(self, config: ContainerConfiguration) -> Host:
         container = self.find_container(config.name)
         if not container:
             self.create_container_folder(config.mount_path)
@@ -42,8 +48,9 @@ class DockerContainerManager:
         else:
             container.start()
             print(f"Container {config.name} already exists.")
-        self.running_containers.append(container)
-        return container
+        self.running_containers[container.id] = config
+        # TODO: Return host with correct inventory file
+        return Host(inventory_path='example', id=container.id)
 
     def launch_all_containers(self, container_configs: list[ContainerConfiguration]):
         # Launch the containers
@@ -52,8 +59,18 @@ class DockerContainerManager:
             container = self.launch_container(config)
             self.running_containers.append(container)
 
-    def stop_all_containers(self):
-        print("Stopping containers...")
-        for container in self.running_containers:
+    def destroy_host(self, host: Host):
+        host_id = host.id
+        container = self.find_container(host_id)
+        config = self.running_containers[host_id]
+        if container:
             container.stop()
-        print("Containers stopped.")
+            print(f"Container {host_id} stopped.")
+        else:
+            print(f"Container {host_id} not found.")
+        shutil.rmtree(config.mount_path, ignore_errors=True)
+        print(f"Folder {config.mount_path} deleted.")
+
+    def host_generator(self) -> Generator[Host, None, None]:
+        for container in self.container_configs:
+            yield self.launch_container(container)
