77b7a3923948529ec07cd81b028f490721ea6478
matteonussbauemer <m.l.nussbaumer@student.tudelft.nl>
2024-05-07 10:33:43 +0200

    add the Deployment as a parameter to the verify_deployment method

--- ansible-runner/test_oracle/test_oracle.py
+++ ansible-runner/test_oracle/test_oracle.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
 from host_manager.host import Host
+from deployment_data import DeploymentData
 
 
 class TestOracle(ABC):
     @abstractmethod
-    def verify_deployment(self, host: Host):
+    def verify_deployment(self, host: Host, deployment_data: DeploymentData):
         pass
