a9fd09ecea3398e0b895c3f3610ff638f69b918c
matteonussbauemer <m.l.nussbaumer@student.tudelft.nl>
2024-05-07 12:36:41 +0200

    create dummy oracle

---
+++ ansible-runner/test_oracle/dummy_test_oracle.py
@@ -0,0 +1,9 @@
+from deployment_data import DeploymentData
+from host_manager.host import Host
+from test_oracle import TestOracle
+from test_result import TestResult
+
+
+class DummyTestOracle(TestOracle):
+    def verify_deployment(self, host: Host, deployment_data: DeploymentData) -> TestResult:
+        return TestResult(True, "Deployment verified successfully")
