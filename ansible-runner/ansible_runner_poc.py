6547366e12af3705c467585eae6fe0ecf1f8e78f
ealnuaimi <ealnuaimi@student.ethz.ch>
2024-05-24 19:30:24 +0200

    Playbook for fedora and fedora commands

--- ansible-runner/ansible_runner_poc.py
+++ ansible-runner/ansible_runner_poc.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
 from test_runner import TestRunner
 from test_oracle.dummy_test_oracle import DummyTestOracle
@@ -14,15 +16,18 @@ if __name__ == "__main__":
         os.makedirs(full_path)
     # Define the configurations for the containers
     container_configs: list[ContainerConfiguration] = [
-        ContainerConfiguration(image="ubuntu:18.04", name="ubuntu_18", mount_path=f"{container_path}/ubuntu_18",
-                               post_init_commands=["apt-get update", "apt-get install -y python3 sudo ufw",
-                                                    "ln -s /usr/bin/python3 /usr/bin/python", "sudo ufw enable",
-                                                    "sudo ufw allow 80/tcp"]),
-        #ContainerConfiguration(image="ubuntu:22.04", name="ubuntu_22", mount_path=f"{container_path}/ubuntu_22",
+        #ContainerConfiguration(image="ubuntu:18.04", name="ubuntu_18", mount_path=f"{container_path}/ubuntu_18",
                                #post_init_commands=["apt-get update", "apt-get install -y python3 sudo ufw",
+                                                    #"ln -s /usr/bin/python3 /usr/bin/python", "sudo ufw enable",
+                                                    #"sudo ufw allow 80/tcp"]),
+        #ContainerConfiguration(image="ubuntu:22.04", name="ubuntu_22", mount_path=f"{container_path}/ubuntu_22",
+                             #post_init_commands=["apt-get update", "apt-get install -y python3 sudo ufw",
                                                    #"ln -s /usr/bin/python3 /usr/bin/python", "sudo ufw enable", 
                                                    #"sudo ufw allow 80/tcp"]),
-        # ContainerConfiguration(image="fedora", name="fedora", mount_path=f"{container_path}/fedora")
+        ContainerConfiguration(image="fedora:39", name="fedora", mount_path=f"{container_path}/fedora", 
+                               post_init_commands=["dnf update -y", "dnf install -y python3 sudo systemd",
+                                    "ln -s /usr/bin/python3 /usr/bin/python", "setsid /usr/sbin/init &"]), 
+                                
     ]
     manager = DockerContainerManager(container_configs=container_configs)
     playbook_path = os.path.join(full_path, "deploy.yml")
